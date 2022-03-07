c39dc964cccf0aaaff87856d92cb80f1d696313e
remss <remss@users.noreply.github.com>
2022-03-07 11:44:04 +0100

    Repro case for electron reload issue

--- main.js
+++ main.js
@@ -8,7 +8,9 @@ function createWindow () {
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
+      nodeIntegration: true,
+      contextIsolation: false,
     }
   })
 
@@ -16,7 +18,7 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
--- renderer.js
+++ renderer.js
@@ -4,3 +4,31 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+
+const fs = require('fs');
+
+async function asyncStuff() {
+    let output = await fs.promises.readFile("datafile");
+    console.log("byteLength", output.byteLength)
+}
+
+function syncStuff() {
+    let output = fs.readFileSync("datafile");
+    console.log("byteLength", output.byteLength)
+}
+
+document.getElementById("reloadMeWithSyncStuff").addEventListener("click", async function () {
+    console.log("reloadMeWithSyncStuff!!!")
+    syncStuff()
+    window.location.reload()
+})
+
+document.getElementById("reloadMeWithAsyncStuff").addEventListener("click", async function () {
+    console.log("reloadMeWithAsyncStuff!!!")
+    asyncStuff()
+    window.location.reload()                             // renderer will crash
+    // window.location.href = "index.html?p=" + Date.now() // renderer will crash
+})
+
+console.log('renderer ready')
